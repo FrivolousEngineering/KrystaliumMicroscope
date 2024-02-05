ce8f97906d99ba993c2e5908acff9eb4af0c2ad6
Arjen Hiemstra <ahiemstra@heimr.nl>
2024-02-05 13:05:38 +0100

    Implement proper refined sample retrieval from API

--- krystalium/api.py
+++ krystalium/api.py
@@ -3,18 +3,27 @@ import logging
 import aiohttp
 import pydantic
 
+
 log = logging.getLogger(__name__)
 
 
 @pydantic.dataclasses.dataclass(kw_only = True, frozen = True)
-class Sample:
-    id: str
+class RefinedSample:
+    id: int
+    rfid_id: str
+
+    primary_action: str
+    primary_target: str
+    secondary_action: str
+    secondary_target: str
+    purity: str
+    purity_score: int
 
 
 @pydantic.dataclasses.dataclass(kw_only = True, frozen = True)
 class Config:
     host: str = "localhost"
-    port: int = 9999
+    port: int = 8000
 
 
 class Api:
@@ -27,17 +36,16 @@ class Api:
     async def stop(self) -> None:
         await self.__session.close()
 
-    async def get(self, id: str) -> Sample | None:
-        async with self.__session.get("/sample") as response:
-            if response.status != 200:
-                logging.warning(f"Could not get data for ID {id}")
+    async def get(self, id: str) -> RefinedSample | None:
+        async with self.__session.get(f"/refined/{id}") as response:
+            if not response.ok:
+                log.warning(f"Could not get data for ID {id}")
                 return None
 
             json = await response.json()
 
-            sample = Sample(
-                id = id,
+            sample = RefinedSample(**json)
 
-            )
+            log.debug(f"Found sample: {sample}")
 
             return sample
