b73df7f938abdd561c87039cd8724e01286e096c
Arjen Hiemstra <ahiemstra@heimr.nl>
2024-03-01 15:43:46 +0100

    Support actual serial in rfid and use a thread
    Now is no longer blocking

--- krystalium/rfid.py
+++ krystalium/rfid.py
@@ -1,8 +1,9 @@
 import os
 import logging
+import threading
 from pathlib import Path
 
-import aiofiles
+import serial
 import pydantic
 
 
@@ -13,36 +14,52 @@ log = logging.getLogger(__name__)
 class Config:
     device_path: str = "rfid"
     use_pipe: bool = True
+    baud_rate: int = 9600
 
 
 class RfidController:
     def __init__(self, config: Config) -> None:
         self.__config = config
+        self.__running = False
+        self.__rfid_id = ""
+        self.__path = None
+
+    @property
+    def rfid_id(self):
+        return self.__rfid_id
 
     async def start(self) -> None:
         if self.__config.use_pipe:
-            path = Path() / self.__config.device_path
-            os.mkfifo(path)
-            self.__fd = os.open(path, os.O_RDONLY)
-            log.info(f"Created FIFO at {path}")
+            self.__path = Path() / self.__config.device_path
+            os.mkfifo(self.__path)
+            log.info(f"Created FIFO at {self.__path}")
         else:
-            self.__fd = os.open(self.__config.device_path, os.O_RDONLY)
+            self.__serial = serial.Serial(self.__config.device_path, self.__config.baud_rate, timeout = 3)
+            if self.__serial is not None:
+                log.info(f"Reading from serial at {self.__config.device_path}")
+
+        self.__running = True
+        self.__thread = threading.Thread(target = self.__detect, daemon=True)
+        self.__thread.start()
 
     async def stop(self) -> None:
-        os.close(self.__fd)
-        if self.__config.use_pipe:
-            os.unlink(Path() / self.__config.device_path)
+        self.__runnng = False
 
-    async def detect(self) -> str | None:
-        async with aiofiles.open(self.__fd, closefd = False) as f:
-            line = await f.readline()
+        if self.__path:
+            os.unlink(self.__path)
 
+    def __detect(self):
+        if self.__path:
+            f = open(self.__path)
+
+        while self.__running:
+            if self.__path:
+                line = f.readline()
+            else:
+                line = self.__serial.readline().decode("utf-8")
+
+            line = line.rstrip()
             if line.startswith("Tag found:"):
-                tag_id = line.replace("Tag found: ", "").strip()
-                log.debug(f"Found tag {tag_id}")
-                return tag_id
+                self.__rfid_id = line.replace("Tag found: ", "")
             elif line.startswith("Tag lost:"):
-                log.debug("Lost tag")
-                return ""
-
-            return None
+                self.__rfid_id = ""
