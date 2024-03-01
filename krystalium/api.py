c24453b936c1716354547b7d0dcb12b31078f7c7
Arjen Hiemstra <ahiemstra@heimr.nl>
2024-03-01 15:42:50 +0100

    Add blood sammple support to api class

--- krystalium/api.py
+++ krystalium/api.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 
 import aiohttp
@@ -5,6 +6,16 @@ import pydantic
 
 
 log = logging.getLogger(__name__)
+
+
+@pydantic.dataclasses.dataclass(kw_only = True, frozen = True)
+class BloodSample:
+    id: int
+    rfid_id: str
+    origin: str
+    strength: int
+    action: str
+    target: str
 
 
 @pydantic.dataclasses.dataclass(kw_only = True, frozen = True)
@@ -36,16 +47,47 @@ class Api:
     async def stop(self) -> None:
         await self.__session.close()
 
-    async def get(self, id: str) -> RefinedSample | None:
+    async def get_samples(self, first_id: str, second_id: str):
+        first_is_blood = False
+        async with self.__session.get(f"/blood/{first_id}") as response:
+            if response.ok:
+                first_is_blood = True
+            else:
+                first_is_blood = False
+
+        blood_id = first_id if first_is_blood else second_id
+        krystal_id = second_id if first_is_blood else first_id
+
+        async with asyncio.TaskGroup() as tg:
+            blood_task = tg.create_task(self.get_blood_sample(blood_id))
+            refined_task = tg.create_task(self.get_refined_sample(krystal_id))
+
+        return blood_task.result(), refined_task.result()
+
+    async def get_blood_sample(self, id: str) -> BloodSample | None:
+        async with self.__session.get(f"/blood/{id}") as response:
+            if not response.ok:
+                log.warning(f"Could not get blood sample with ID {id}")
+                return None
+
+            json = await response.json()
+
+            sample = BloodSample(**json)
+
+            log.debug(f"Found blood sample: {sample}")
+
+            return sample
+
+    async def get_refined_sample(self, id: str) -> RefinedSample | None:
         async with self.__session.get(f"/refined/{id}") as response:
             if not response.ok:
-                log.warning(f"Could not get data for ID {id}")
+                log.warning(f"Could not get refined sample with ID {id}")
                 return None
 
             json = await response.json()
 
             sample = RefinedSample(**json)
 
-            log.debug(f"Found sample: {sample}")
+            log.debug(f"Found refined sample: {sample}")
 
             return sample
