bebaae356411e824f8ab960fdd5063529ed21460
Arjen Hiemstra <ahiemstra@heimr.nl>
2024-03-01 15:47:05 +0100

    Implement more of the modifier logic

--- krystalium/medical.py
+++ krystalium/medical.py
@@ -33,46 +33,57 @@ class Color:
         )
 
 
-@dataclass(kw_only = True, frozen = True)
+@dataclass(kw_only = True)
 class SystemParameters:
-    rbc_count: int | None = Field(title = "RBC Count", default = None)
-    rbc_tint: Color | None = Field(title = "RBC Tint", default = None)
-    rbc_movement_multiplier: float | None = Field(title = "RBC Movement Multiplier", default = None)
-    rbc_scale: float | None = Field(title = "RBC Scale", default = None)
+    base_spawn_rate: float = Field(title = "Base Spawn Rate", default = 10.0)
+    base_movement_speed: float = Field(title = "Base Movement", default = 6.0)
+    base_movement_jitter: float = Field(title = "Base Movement Jitter", default = 0.0)
+    base_color: Color = Field(title = "Base Color", default = Color(r = 0.7, g = 0.7, b = 0.7, a = 1.0))
 
-    wbc_count: int | None = Field(title = "WBC Count", default = None)
-    wbc_tint: Color | None = Field(title = "WBC Tint", default = None)
-    wbc_movement_multiplier: float | None = Field(title = "WBC Movement Multiplier", default = None)
-    wbc_scale: float | None = Field(title = "WBC Scale", default = None)
+    rbc_spawn_chance: float = Field(title = "RBC Spawn Chance", default = 0.5)
+    rbc_tint: Color = Field(title = "RBC Tint", default = Color(r = 0.0, g = 0.0, b = 0.0, a = 0.0))
+    rbc_scale: float = Field(title = "RBC Scale", default = 1.0)
+    rbc_movement_multiplier: float = Field(title = "RBC Movement Multiplier", default = 1.0)
+    rbc_movement_jitter: float = Field(title = "RBC Movement Jitter", default = 0.0)
+    rbc_normal_chance: float = Field(title = "RBC Normal Chance", default = 1.0)
+    rbc_helmet_chance: float = Field(title = "RBC Helmet Chance", default = 0.0)
+    rbc_burr_chance: float = Field(title = "RBC Burr Chance", default = 0.0)
+    rbc_oval_chance: float = Field(title = "RBC Oval Chance", default = 0.0)
 
-    platelet_count: int | None = Field(title = "Platelet Count", default = None)
-    platelet_tint: Color | None = Field(title = "Platelet Tint", default = None)
-    platelet_movement_multiplier: float | None = Field(title = "Platelet Movement Multiplier", default = None)
-    platelet_scale: float | None = Field(title = "Platelet Scale", default = None)
+    wbc_spawn_chance: float = Field(title = "WBC Spawn Chance", default = 0.01)
+    wbc_tint: Color = Field(title = "WBC Tint", default = Color(r = 0.0, g = 0.0, b = 0.0, a = 0.0))
+    wbc_scale: float = Field(title = "WBC Scale", default = 1.0)
+    wbc_movement_multiplier: float = Field(title = "WBC Movement Multiplier", default = 2.0)
+    wbc_movement_jitter: float = Field(title = "WBC Movement Jitter", default = 0.0)
 
-    movement_speed: float | None = Field(title = "Movement Speed", default = None)
+    platelet_spawn_chance: float | None = Field(title = "Platelet Spawn Chance", default = 0.1)
+    platelet_tint: Color = Field(title = "Platelet Tint", default = Color(r = 0.0, g = 0.0, b = 0.0, a = 0.0))
+    platelet_scale: float = Field(title = "Platelet Scale", default = 1.0)
+    platelet_movement_multiplier: float = Field(title = "Platelet Movement Multiplier", default = 0.75)
+    platelet_movement_jitter: float = Field(title = "Platelet Movement Jitter", default = 0.0)
 
-    @classmethod
-    def merged(first, second):
-        def merge(first, second, op):
-            if first is None and second is None:
-                return None
+    strand_spawn_chance: float | None = Field(title = "Coagulated Strand Spawn Chance", default = 0.0)
+    strand_tint: Color = Field(title = "Coagulated Strand Tint", default = Color(r = -0.2, g = -0.2, b = -0.2, a = 0.0))
+    strand_scale: float = Field(title = "Coagulated Strand Scale", default = 1.0)
+    strand_lifetime: float = Field(title = "Coagulated Strand Lifetime", default = 1000.0)
+    strand_movement: float = Field(title = "Coagulated Strand Movement", default = 0.03)
 
-            if first is not None and second is None:
-                return first
+    krystal_spawn_chance: float = Field(title = "Krystal Spawn Chance", default = 0.0)
+    krystal_tint: Color = Field(title = "Krystal Tint", default = Color(r = -0.4, g = -0.3, b = 2.0, a = 0.0))
+    krystal_scale: float = Field(title = "Krystal Scale", default = 1.0)
+    krystal_movement_multiplier: float = Field(title = "Krystal Movement Multiplier", default = 1.0)
+    krystal_movement_jitter: float = Field(title = "Krystal Movement Jitter", default = 0.0)
 
-            if first is None and second is not None:
-                return second
+    plant_spawn_chance: float = Field(title = "Plant Spawn Chance", default = 0.0001)
+    plant_tint: Color = Field(title = "Plant Tint", default = Color(r = -0.2, g = -0.1, b = -0.2, a = 0.0))
+    plant_scale: float = Field(title = "Plant Scale", default = 1.0)
+    plant_movement_multiplier: float = Field(title = "Plant Movement Multiplier", default = 1.0)
+    plant_movement_jitter: float = Field(title = "Plant Movement Jitter", default = 0.0)
 
-            return op(first, second)
-
-        params = SystemParameters()
-
-        params.rbc_count = merge(first.rbc_count, second.rbc_count, lambda a, b: a + b)
-        params.rbc_tint = merge(first.rbc_tint, second.rbc_tint, Color.mix)
-        params.rbc_movement_multiplier = merge(first.rbc_movement_multiplier, second.rbc_movement_multiplier, lambda a, b: a + b)
-
-        return params
+    dead_spawn_chance: float = Field(title = "Dead Cell Spawn Chance", default = 0.0001)
+    dead_tint: Color = Field(title = "Dead Cell Tint", default = Color(r = 0.0, g = 0.0, b = 0.0, a = 0.0))
+    dead_scale: float = Field(title = "Dead Cell Scale", default = 1.0)
+    dead_movement_multiplier: float = Field(title = "Dead Cell Movement Multiplier", default = 1.0)
 
 
 class UnrealCommunication:
@@ -81,6 +92,22 @@ class UnrealCommunication:
 
     def __init__(self, config: Config) -> None:
         self.__config = config
+        self.__active = False
+        self.__actual_level_object_path = ""
+
+    @property
+    def active(self) -> bool:
+        return self.__active
+
+    async def set_active(self, active: bool) -> None:
+        if active == self.__active:
+            return
+
+        self.__active = active
+        if self.__active:
+            await self.play_startup()
+        else:
+            await self.play_stop()
 
     async def start(self):
         self.__session = aiohttp.ClientSession(f"http://{self.__config.host}:{self.__config.port}")
@@ -89,17 +116,25 @@ class UnrealCommunication:
         await self.play_stop()
         await self.__session.close()
 
-    async def update_from_sample(self, sample):
-        parameters = effect_table.get(sample.primary_action, {}).get(sample.primary_target, None)
-        if not parameters:
-            return
+    async def update_from_samples(self, blood_sample, krystal_sample):
+        parameters = SystemParameters()
 
-        secondary_parameters = effect_table.get(sample.secondary_action, {}).get(sample.secondary_target, None)
-        if secondary_parameters:
-            parameters = SystemParameters.merged(parameters, secondary_parameters)
+        blood_modifiers = effect_table.get(blood_sample.action, {}).get(blood_sample.target, [])
+        if not blood_modifiers:
+            log.warning(f"Unknown blood sample origin: {blood_sample.origin}")
+        self.apply_modifiers(parameters, blood_modifiers, blood_sample.strength)
+
+        primary_modifiers = effect_table.get(krystal_sample.primary_action, {}).get(krystal_sample.primary_target, [])
+        if not primary_modifiers:
+            log.warning(f"Unknown action/target combination: {krystal_sample.primary_action}/{krystal_sample.primary_target}")
+        self.apply_modifiers(parameters, primary_modifiers, krystal_sample.purity_score)
+
+        secondary_modifiers = effect_table.get(krystal_sample.secondary_action, {}).get(krystal_sample.secondary_target, [])
+        if not secondary_modifiers:
+            log.warning(f"Unknown action/target combination: {krystal_sample.secondary_action}/{krystal_sample.secondary_target}")
+        self.apply_modifiers(parameters, secondary_modifiers, krystal_sample.purity_score)
 
         batch = []
-
         for field in dataclasses.fields(parameters):
             value = getattr(parameters, field.name, None)
             if value is None:
@@ -156,10 +191,25 @@ class UnrealCommunication:
             else:
                 return True
 
-    @staticmethod
-    def __toNiagara(python_type, value):
-        source_type = typing.get_args(python_type)[0]
+    def apply_modifiers(self, parameters: SystemParameters, modifiers: list[ParameterModifier], strength: int):
+        for modifier in modifiers:
+            current_value = getattr(parameters, modifier.parameter)
+            modifier_strength = 1.0 + ((strength - 2) / 10) * 0.5
 
+            new_value = current_value
+            if modifier.operation == "add":
+                new_value = current_value + modifier.value * modifier_strength
+            elif modifier.operation == "mul":
+                new_value = current_value * (modifier.value * modifier_strength)
+            elif modifier.operation == "set":
+                new_value = modifier.value * modifier_strength
+            else:
+                log.warning(f"Unknown modifier operation: {modifier.operation}")
+
+            setattr(parameters, modifier.parameter, new_value)
+
+    @staticmethod
+    def __toNiagara(source_type, value):
         if source_type == bool:
             return "SetNiagaraVariableBool", bool(value)
         elif source_type == int:
